7ace8da53726108e26b5a2fd6ab1a7244beae38c
china555 <kittikorn.kee@student.mahidol.edu>
2020-06-01 20:19:30 +0700

    login

---
+++ day1/src/views/Home.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <!-- <span v-if="seen">Now you can see</span>
+    <span v-if="test">HelloWorld</span>
+    <li v-for="(todo, index) in todos" :key="`todo-${index}`">{{ todo.text }} {{ index }}</li>
+    <p>Using mustaches: {{ rawHtml }}</p>
+    <p>
+      Using v-h tml directive:
+      <span style="color: red" v-html="rawHtml"></span>
+    </p>
+    <div>{{ msg }}</div>
+    <div>{{ reversedMsg }}</div>
+    <span>{{ now }}</span>
+    <div>{{ reversedMsg2() }}</div>
+    <span>{{ now2() }}</span>
+    <div>{{ fullName }}</div>
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh</h1>
+    <div v-if="Math.random() > 0.5">Now you can see</div>
+    <div v-else>Now you don't</div>
+    <ul v-for="(set,index) in sets" :key="`set-${index}`">
+      <li v-for="(n,index) in even(set)" :key="`n-${index}`">{{n}}</li>
+    </ul>-->
+    <select v-model="select">
+      <option v-for="option in options" :key="option.value">{{option.text}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      seen: true,
+      test: true,
+      todos: [{ text: "Learning" }, { text: "Hello" }, { text: "Woeld" }],
+      rawHtml: "adwad",
+      msg: "Chinaa",
+      firstname: "Kittikorn",
+      lastname: "Keeratikriengkrai",
+      fullName: "Kittikorn Keeratikriengkrai",
+      awesome: true,
+      sets: [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 0]
+      ],
+      options: [
+        { text: "One", value: "A" },
+        { text: "Two", value: "B" },
+        { text: "Three", value: "C" }
+      ],
+      select: "A"
+    };
+  },
+  computed: {
+    reversedMsg: function() {
+      return this.msg
+        .split("")
+        .reverse()
+        .join("");
+    },
+    now: function() {
+      return Date.now();
+    }
+  },
+  methods: {
+    reversedMsg2: function() {
+      return this.msg
+        .split("")
+        .reverse()
+        .join("");
+    },
+    now2: function() {
+      return Date.now();
+    },
+    even: function(numbers) {
+      return numbers.filter(function(number) {
+        return number % 2 === 0;
+      });
+    }
+  },
+  watch: {
+    firstname: function(val) {
+      this.fullName = val + " " + this.lastname;
+    }
+  },
+  components: {
+    // HelloWorld
+  }
+};
+</script>
---
+++ day1/src/views/login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <div class="headddd">login</div>
+      <div class="login_option">
+        <a @click="email">Email</a>
+        <a @click="phone">Phone</a>
+      </div>
+      <div class="space">
+        <div class="inputtext">{{check}}</div>
+        <input type="text" />
+        <div class="inputtext">Password</div>
+        <input type="text" />
+      </div>
+      <div>
+        <button @click="submit" class="btn">submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { check: "Email" };
+  },
+  methods: {
+    email: function() {
+      this.check = "Email";
+    },
+    phone: function() {
+      this.check = "Phone Number";
+    },
+    submit: function() {
+      alert("submit already");
+    }
+  }
+};
+</script>
+<style scoped>
+.btn {
+  border-radius: 5px;
+}
+.space {
+  margin: 30px 0;
+}
+.inputtext {
+  text-align: left;
+  margin: 30px 0 10px 15%;
+  color: aliceblue;
+}
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  background-color: rgb(255, 255, 255);
+}
+input {
+  width: 70%;
+}
+.box {
+  width: 40%;
+  flex-wrap: wrap;
+  background-color: rgba(0, 0, 0, 0.83);
+  justify-content: center;
+  padding: 10px;
+}
+.headddd {
+  width: 100%;
+  color: aliceblue;
+  font-size: 30px;
+}
+.login_option > a {
+  color: aliceblue;
+  text-decoration: none;
+  margin: 20px;
+}
+.login_option > a:hover {
+  cursor: pointer;
+  color: rgb(5, 16, 49);
+}
+.login_option {
+  margin-top: 20px;
+  width: 100%;
+}
+</style>
