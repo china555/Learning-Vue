91cbd9996018edb91daea2a6c52fc4710444583f
china555 <kittikorn.kee@student.mahidol.edu>
2020-06-02 23:32:03 +0700

    Basic Component

---
+++ day2/vue/src/components/testcomponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <div class="titleCard">{{title}}</div>
+    <div class="text">{{text}}</div>
+  </div>
+</template>
+<script scope>
+export default {
+  props: ["title", "text"]
+};
+</script>
+<style scoped>
+.card {
+  display: flex;
+  flex-wrap: wrap;
+  background-color: rgb(214, 24, 135);
+  justify-content: center;
+  width: 20vw;
+  border-radius: 10px;
+  margin: 50px;
+}
+.titleCard {
+  color: rgb(245, 226, 226);
+  font-size: 30px;
+  width: 100%;
+  font-weight: 700;
+}
+.text {
+  padding: 10px;
+  color: rgb(245, 226, 226);
+  font-size: 16px;
+  word-break: break-all;
+}
+</style>
---
+++ day2/vue/src/views/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <router-link to="/">Click me!!!</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return;
+  }
+};
+</script>
+<style  scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.box {
+  width: 50%;
+  background-color: blueviolet;
+}
+</style>
