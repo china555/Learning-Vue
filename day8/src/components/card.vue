957c7cdc3b05d2cc894d17063f78d8eb0f8879c9
china <46339031+china555@users.noreply.github.com>
2020-06-17 16:42:19 +0700

    1

---
+++ day8/src/components/card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="card-area">
+      <div class="card">awdawdwad</div>
+    </div>
+    <div class="card-area">
+      <div class="card">awdawdwad</div>
+    </div>
+    <div class="card-area">
+      <div class="card">awdawdwad</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scope>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.card-area {
+  width: 33%;
+  display: flex;
+  justify-content: center;
+}
+.card {
+  background-color: rgb(139, 214, 233);
+  margin: 5vh 2vw;
+  padding: 10px 10% 20% 10%;
+  border-radius: 6px;
+}
+.content-card {
+  color: white;
+}
+</style>
