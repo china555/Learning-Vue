97bb1ed0eba4d07df0d18940c64cbd09ff261c93
china <46339031+china555@users.noreply.github.com>
2020-06-18 15:10:42 +0700

    Design Schedule

--- day9/src/App.vue
+++ day9/src/App.vue
@@ -11,12 +11,106 @@
           <div class="view-all">view all</div>
         </div>
         <card
+          areaPicture="left-side-area"
+          card="card"
           title="Call doctor for tests"
-          datetime="15 Mar 2020 - 9:00AM"
-          Discription="Ask for blood tests and GYM certificate."
+          time="9:00AM"
+          Datee="15 Mar 2020"
+          Discription="Ask for blood tests and GYM certificate"
+          leftside="left-side"
         />
-        <card />
-        <card />
+        <card
+          areaPicture="left-side-area"
+          card="card"
+          title="Beatrice's bday"
+          Datee="22 Mar 2020"
+          time="12:00PM"
+          leftside="left-side"
+        />
+
+        <card
+          areaPicture="left-side-area"
+          title="Add new weekly pin"
+          card="card"
+          leftside="left-side"
+        />
+      </div>
+    </div>
+    <div class="content-right space">
+      <div class="container justify-space-between">
+        <div class="header_text">Today's schedule</div>
+        <buttonplus :size="30" />
+      </div>
+      <div class="container">
+        <div class="header_text" style="color: #eea735;">Thursday 11</div>
+        <div class="arrow-area container">
+          <div class="area-arrow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="black"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-arrow-left"
+            >
+              <line x1="19" y1="12" x2="5" y2="12" />
+              <polyline points="12 19 5 12 12 5" />
+            </svg>
+          </div>
+          <div class="area-arrow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="black"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-arrow-right"
+            >
+              <line x1="5" y1="12" x2="19" y2="12" />
+              <polyline points="12 5 19 12 12 19" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="content-middle-area">
+        <card
+          areaPicture="left-side-area-middle"
+          card="card2"
+          title="Wake up Buddy"
+          leftside="left-side2"
+          Datee="22 Mar 2020"
+          time="12:00PM"
+        />
+        <card
+          areaPicture="left-side-area-middle"
+          card="card2"
+          title="Morning Yoga"
+          leftside="left-side2"
+          Datee="22 Mar 2020"
+          time="8:00AM"
+        />
+        <card
+          areaPicture="left-side-area-middle"
+          card="card3"
+          title="Morning Yoga"
+          leftside="left-side2"
+          Datee="22 Mar 2020"
+          time="8:00AM"
+        >
+          <ul>
+            <li>Squat 10x3</li>
+            <li>Push up 10x3</li>
+            <li>Push up 10x3</li>
+          </ul>
+        </card>
       </div>
     </div>
   </div>
@@ -34,12 +128,19 @@ export default {
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Rubik&display=swap");
 * {
-  font-family: "Roboto", sans-serif;
+  /* font-family: "Archivo", sans-serif; */
+  font-family: "Rubik", sans-serif;
 }
 body {
   margin: 0;
+}
+.content-middle-area {
+  margin: 40px 0 0 35px;
+}
+.justify-space-between {
+  justify-content: space-between;
 }
 .container {
   display: flex;
@@ -48,17 +149,23 @@ body {
 .head {
   color: rgb(14, 7, 43);
 }
-
+.header_text {
+  font-size: 32px;
+}
 .content-left {
   width: 30vw;
   min-height: 100vh;
   background-color: #eaeef0;
+  padding-top: 2vh;
+}
+.content-right {
+  width: 62.97vw;
 }
 .content-left-middle {
   margin-top: 10%;
 }
 .space {
-  margin: 7%;
+  margin: 3%;
 }
 .text-header {
   margin: 5px 0 0 10px;
@@ -71,7 +178,19 @@ body {
 }
 .view-all {
   margin-top: 5px;
-  margin-left: 50%;
+  margin-left: 40%;
   color: #eea735;
+}
+.arrow-area {
+  margin-left: 40px;
+}
+.area-arrow {
+  background-color: rgb(240, 240, 240);
+  margin-top: 5px;
+  margin-right: 20px;
+  border-radius: 15px;
+  justify-content: center;
+  height: 60%;
+  padding: 3px;
 }
 </style>
--- day9/src/components/card.vue
+++ day9/src/components/card.vue
@@ -1,16 +1,54 @@
 <template>
-  <div class="card" @click="expand">
+  <div :class="card" @click="expand">
     <div class="container">
-      <div class="left-side">
-        <div class="left-side-area">
-          <img src="https://picsum.photos/768" alt />
+      <div :class="leftside">
+        <div :class="areaPicture">
+          <img
+            src="https://picsum.photos/768"
+            class="classimg"
+            v-if="Datee != null && time != null "
+            alt
+          />
+          <svg
+            class="feather feather-plus classimg"
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            :width="30"
+            :height="30"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#ffffff"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="12" y1="5" x2="12" y2="19" />
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
         </div>
       </div>
       <div class="right-side">
-        <div class="right-side-area">{{title}}</div>
-        <div class="expand" v-if="check === true ">
-          <div>{{Datetime}}</div>
-          <div class="Personal">Personal</div>
+        <div class="container3" v-if="check === false && card === 'card'">
+          <div class="right-side-area">{{title}}</div>
+          <div class="container" v-if="check === false && card === 'card' ">{{Datee}}</div>
+          <div class="container" v-if="card === 'card2'">{{time}}</div>
+        </div>
+        <div class="container2" v-if="check === true && card === 'card' ">
+          <div class="right-side-area">{{title}}</div>
+          <div class="container" v-if="check === false && card === 'card' ">{{Datee}}</div>
+          <div class="container" v-if="card === 'card2'">{{time}}</div>
+        </div>
+
+        <div class="container3" v-if="card === 'card2'||card === 'card3'">
+          <div class="right-side-area">{{title}}</div>
+          <div class="container">{{time}}</div>
+        </div>
+        <slot />
+        <div class="container" v-if="check === true && card === 'card' ">
+          <div v-if="Datee != null">{{Datee}}-{{time}}</div>
+          <div class="per">
+            <div class="Personal">Personal</div>
+          </div>
           <div class="discription">{{Discription}}</div>
         </div>
       </div>
@@ -24,8 +62,12 @@ export default {
   },
   props: {
     title: String,
-    Datetime: String,
-    Discription: String
+    Datee: String,
+    time: String,
+    Discription: String,
+    areaPicture: String,
+    card: String,
+    leftside: String
   },
   methods: {
     expand: function() {
@@ -36,8 +78,20 @@ export default {
 };
 </script>
 <style scope>
+.card2 {
+  margin-top: 4%;
+  background-color: #eea735b0;
+  padding: 20px;
+  border-radius: 10px;
+}
 .card {
-  margin-top: 5%;
+  margin-top: 7%;
+  background-color: rgb(247, 247, 247);
+  padding: 20px;
+  border-radius: 10px;
+}
+.card3 {
+  margin-top: 4%;
   background-color: rgb(247, 247, 247);
   padding: 20px;
   border-radius: 10px;
@@ -45,18 +99,39 @@ export default {
 .card:hover {
   background-color: rgba(173, 171, 171, 0.685);
   cursor: pointer;
+  box-shadow: 5px 8px 9px 5px rgb(223, 223, 223);
 }
 .left-side {
   width: 20%;
   min-height: 100%;
 }
-img {
+.left-side2 {
+  width: 5.5%;
+  min-height: 100%;
+  margin-right: 20px;
+}
+.container3 {
+  margin-top: 13px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.container2 {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.classimg {
   padding: 7px 2px 4px 7px;
   width: 35px;
 }
 .left-side-area {
-  width: 70%;
+  width: 65%;
   background-color: #eea735b0;
+  border-radius: 7px;
+}
+.left-side-area-middle {
+  background-color: #ffffff;
   border-radius: 7px;
 }
 .right-side {
@@ -66,11 +141,8 @@ img {
   font-size: 19px;
   font-weight: 600;
 }
-.expand {
-  /* display: none; */
-}
 .Personal {
-  width: 20%;
+  width: 16%;
   margin: 20px 0;
   padding: 3px 15px;
   background-color: #eea735b0;
@@ -80,7 +152,10 @@ img {
 }
 .discription {
   width: 80%;
-  font-size: 12px;
+  font-size: 15px;
   word-wrap: break-word;
+}
+.per {
+  width: 100%;
 }
 </style>
